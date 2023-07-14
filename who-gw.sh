fcff319edaa1edfb0dc1b2bec0e7d64305c689ca
Vladimir <sikpro1337@yandex.ru>
2023-07-14 16:20:07 +0300

    Добавлены комментарии к коду.

--- who-gw.sh
+++ who-gw.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Делаем вывод юзера через переменную командой whoami
 username=$(whoami)
 echo "User: $username"
 
+
+# Выводит шлюз по умолчанию через ip route и вырезкой через awk нужного столбца
 gateway=$(ip route | awk '/default/ { print $3 }')
 echo "Gateway: $gateway"
