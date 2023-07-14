dd7882f9a29c473b3efd60ca315abc6900ff1cf4
Vladimir <sikpro1337@yandex.ru>
2023-07-14 16:48:45 +0300

    Добавлен скрипт вывода локального айпи адреса.

--- who-gw.sh
+++ who-gw.sh
@@ -4,7 +4,11 @@
 username=$(whoami)
 echo "User: $username"
 
+# Вызывает команду ip a с сортировкой на локальный айпи адрес.
+ip=$(ip a | grep 'inet ' | awk '{print $2}' | sed -n '2p')
+echo "IP address: $ip"
 
 # Выводит шлюз по умолчанию через ip route и вырезкой через awk нужного столбца
 gateway=$(ip route | awk '/default/ { print $3 }')
 echo "Gateway: $gateway"
+
